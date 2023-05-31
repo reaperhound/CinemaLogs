610c210533388be956c1711a080eb1fb47dff226
reaperhound <reaperhound69004@gmail.com>
2023-05-31 11:57:04 +0530

    Added logo and icons to sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useGetGenresQuery } from "../../services/TMDB";
+import ReactLoading from "react-loading";
+import genreIcons from "../../assets/index";
 
 const Sidebar = () => {
+  const { data, isFetching } = useGetGenresQuery();
+  console.log("genres", data);
   const categories = [
     { label: "Popular", value: "popular" },
     { label: "Top Rated", value: "top_rated" },
@@ -14,17 +19,23 @@ const Sidebar = () => {
     { label: "Animation", value: "aniation" },
   ];
   return (
-    <div className="bg-base-200 animate-fade-right h-[200vh]  duration-300 z-50 text-base-content border border-gray-500 text-3xl w-[240px] flex flex-col gap-5 justify-start min-h-[100vh] absolute left-0">
-      <h1 className="py-6 mx-auto animate-pulse">Logo</h1>
+    <div className="bg-base-200 animate-fade-right pb-32  duration-300 z-50 text-base-content border border-gray-500 text-3xl w-[240px] flex flex-col gap-5 justify-start min-h-[100vh] absolute left-0">
+      <h1 className="py-6 mx-auto animate-spin animate-once">
+        <img src={genreIcons.logo} alt="logo" className="w-1/3 mx-auto" />
+      </h1>
 
-      {/* Categories */}
-      <h2 className="text-lg pl-7 ">Categories</h2>
+      {/* Genres */}
+      <h2 className="text-lg pl-7 ">Genres</h2>
       <div>
         {categories.map(({ label, value }) => (
-          <Link key={value} to={"/"} className="flex justify-start my-4 pl-3">
+          <Link
+            key={value}
+            to={"/"}
+            className="flex justify-start my-4 pl-3 py-3 hover:bg-primary-focus hover:text-primary-content ease-in-out duration-100"
+          >
             <div className="avatar placeholder mx-2">
-              <div className="bg-white text-neutral-content rounded-full w-8">
-                <span className="text-xs">&</span>
+              <div className="bg-inherit text-neutral-content rounded-full w-8 mr-2">
+                <img src={genreIcons[label.toLowerCase()]} alt={name} />
               </div>
             </div>
             <p className="text-lg">{label}</p>
@@ -32,19 +43,27 @@ const Sidebar = () => {
         ))}
       </div>
 
-      {/* Genres */}
-      <h2 className="text-lg pl-7 ">Genres</h2>
-      <div>
-        {demoCategories.map(({ label, value }) => (
-          <Link key={value} to={"/"} className="flex justify-start my-4 pl-3">
-            <div className="avatar placeholder mx-2">
-              <div className="bg-white text-neutral-content rounded-full w-8">
-                <span className="text-xs">&</span>
+      {/* Categories */}
+      <h2 className="text-lg pl-7 ">Categories</h2>
+      <div className="text-center">
+        {isFetching ? (
+          <ReactLoading type="bubbles" color="#f9d72f" />
+        ) : (
+          data.genres.map(({ name, id }) => (
+            <Link
+              key={id}
+              to={"/"}
+              className="flex justify-start my-4 pl-3 py-3 hover:bg-primary-focus hover:text-primary-content ease-in-out duration-100"
+            >
+              <div className="avatar placeholder mx-2">
+                <div className="bg-inherit text-neutral-content rounded-full w-8 mr-2">
+                  <img src={genreIcons[name.toLowerCase()]} alt={name} />
+                </div>
               </div>
-            </div>
-            <p className="text-lg">{label}</p>
-          </Link>
-        ))}
+              <p className="text-lg">{name}</p>
+            </Link>
+          ))
+        )}
       </div>
     </div>
   );
